e533b10d6ce560e985005db10df51cede6ad960e
rjsaurav13 <rjsaurav13@gmail.com>
2022-02-10 00:36:00 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportActionBar().hide(); // hide the title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         TextView t1 = (TextView) findViewById(R.id.textView6);
         t1.setMovementMethod(LinkMovementMethod.getInstance());

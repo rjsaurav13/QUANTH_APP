5bfcadca51c3bab461c811e6ab41dbc7266a68dd
rjsaurav13 <rjsaurav13@gmail.com>
2022-02-11 17:03:08 +0530

    Update MainActivity2.java

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -19,7 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main2);
-        getSupportActionBar().hide(); // hide the title bar
+        getSupportActionBar().hide(); // hide the title barGG
 
 //        Button signup = findViewById(R.id.button1);
         Button login= findViewById(R.id.button2);

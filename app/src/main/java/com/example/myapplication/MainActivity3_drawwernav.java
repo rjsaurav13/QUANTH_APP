38dca96fc9ee853348618ce449ab38898cf63af3
rjsaurav13 <rjsaurav13@gmail.com>
2022-02-13 23:26:32 +0530

    Create MainActivity3_drawwernav.java

---
+++ app/src/main/java/com/example/myapplication/MainActivity3_drawwernav.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3_drawwernav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity3_drawwernav);
+    }
+}

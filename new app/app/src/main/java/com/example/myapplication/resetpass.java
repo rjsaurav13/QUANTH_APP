7db149d188391da62233fa875a81696d9e2139c0
rjsaurav13 <rjsaurav13@gmail.com>
2022-02-20 02:24:36 +0530

    Update resetpass.java

--- new app/app/src/main/java/com/example/myapplication/resetpass.java
+++ new app/app/src/main/java/com/example/myapplication/resetpass.java
@@ -2,22 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.os.Bundle;
-
 public class resetpass extends AppCompatActivity {
-    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpass);
-
-        button = (Button) findViewById(R.id.button2);
+       Button button = (Button) findViewById(R.id.button2);
 
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -32,3 +27,4 @@ public class resetpass extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
